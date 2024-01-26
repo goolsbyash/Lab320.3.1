5e72ec615db82cf927f9d173d70dd6abccbd443d
goolsbyash <goolsbyashley015@gmail.com>
2024-01-26 14:45:18 -0500

    Listed Learner component for rendering.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { learnerData } from "./data/learnerData";
+import Learner from "./Components/Learner";
+import { useState } from "react";
 
 function App() {
+  const [learners, setLearners] = useState(learnerData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="learner-app">
+      <h1> Learner App</h1>
+      <Learner learners={learners}/>
     </div>
   );
 }
